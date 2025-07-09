2a8dc1301aceb5f17f607a00ab775b3995ee8b6f
Oued25 <o.issoufu@gmail.com>
2025-07-09 04:37:47 +0000

    commit - mise à jours dU  fichiers connexion INCLUDESs

--- includes/connection.php
+++ includes/connection.php
@@ -1,7 +1,7 @@
 <?php  
 session_start();// une fonction qui permet d'afficher les messages d'erreur de connexion en utilisant une varible  $_SESSION
                 // il a ete placer à ce niveau parce tout comme le ficher connexion.php, il sera appler dans tous les qutre ficher du code
-    $nom_serveur = "62.171.183.84";
+    $nom_serveur = "127.0.0.1";
     $db = "eFuelPrepaid_DEV2";
     $utilisateur = "ubuntu";  
     $motpass = "bUDhKxE4$)oSQ";
